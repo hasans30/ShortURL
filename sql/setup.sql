baae1ecf4bc15511d84188db56fc59683bba518c
Austin Heiman <atheiman@ksu.edu>
2014-08-27 17:57:16 -0500

    html work

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,33 +1,14 @@
-use app_ly;
+use shorturl;
 -- drop old tables
 drop table links;
-drop table users;
-
--- create new tables
-create table users
-(
-user_email varchar(255) not null,
-password varchar(255) not null,
-date_joined datetime not null,
-primary key (user_email)
-);
 
 create table links
 (
 shorturl_id int not null auto_increment,
 longurl varchar(255) not null,
 date_created datetime not null,
-user_email varchar(255),
 primary key (shorturl_id),
-foreign key (user_email) references users(user_email)
 );
-
--- insert into new tables
-insert into users ( user_email , password , date_joined )
-values
-( 'austin@example.com' , 'austinpass' , NOW() ),
-( 'chris@example.com' , 'chrispass' , NOW() )
-;
 
 insert into links ( longurl , date_created , user_email )
 values
